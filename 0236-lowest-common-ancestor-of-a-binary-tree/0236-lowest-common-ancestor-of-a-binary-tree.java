b12842349e251894d7ae8b01a4a9e9ec22efb7fc
Ramm_Lakshmanan <rockingramm27@gmail.com>
2025-12-06 00:10:52 +0530

    Time: 7 ms (81.91%), Space: 46.3 MB (42.73%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -1,35 +1,15 @@
-class pair {
-    boolean f1, f2;
-    TreeNode node;
-
-    public pair(TreeNode node, boolean f1, boolean f2) {
-        this.node = node;
-        this.f1 = f1;
-        this.f2 = f2;
-    }
-}
-
 class Solution {
-    public pair lca(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null) return new pair(null, false, false);
+    public TreeNode lca(TreeNode root,TreeNode p,TreeNode q){
+        if(root==null || root==p || root==q) return root;
 
-        pair left = lca(root.left, p, q);
-        // If left subtree already found LCA, return immediately
-        if (left.node != null && left.f1 && left.f2) return left;
+        TreeNode left=lca(root.left,p,q);
+        TreeNode right=lca(root.right,p,q);
 
-        pair right = lca(root.right, p, q);
-        // If right subtree already found LCA, return immediately
-        if (right.node != null && right.f1 && right.f2) return right;
-
-        boolean f1 = left.f1 || right.f1 || (root == p);
-        boolean f2 = left.f2 || right.f2 || (root == q);
-
-        TreeNode lcaNode = (f1 && f2) ? root : null;
-
-        return new pair(lcaNode, f1, f2);
+        if(left==null) return right;
+        else if(right==null) return left;
+        else return root;
     }
-
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        return lca(root, p, q).node;
+        return lca(root,p,q);
     }
 }
