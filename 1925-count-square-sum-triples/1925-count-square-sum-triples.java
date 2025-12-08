0c93a9bcbaccd458c4b72b5c9c3f42b7348db088
Ramm_Lakshmanan <rockingramm27@gmail.com>
2025-12-08 14:22:01 +0530

    Time: 29 ms (29.85%), Space: 41.9 MB (59.5%) - LeetHub

---
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int countTriples(int n) {
+        int[] sqrs=new int[n];
+        for(int i=1;i<=n;i++) sqrs[i-1]=(i*i);
+        int cnt=0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                int sq=(i*i)+(j*j);
+                if(Arrays.binarySearch(sqrs,sq)>=0) cnt++;
+            }
+        }
+        return cnt;
+    }
+}
