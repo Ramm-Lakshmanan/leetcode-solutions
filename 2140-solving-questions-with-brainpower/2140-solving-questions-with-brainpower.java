30a8b816dd1d0f6e7fdf248910ede8cc8cd3e146
Ramm_Lakshmanan <rockingramm27@gmail.com>
2025-12-20 14:36:57 +0530

    Time: 7 ms (51.12%), Space: 140.5 MB (56.55%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.java
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.java
@@ -1,5 +1,5 @@
 class Solution {
-    public long ans(int n,int[][] questions,long[] dp){
+    /*public long ans(int n,int[][] questions,long[] dp){
         if(n==questions.length-1) return (long)questions[n][0];
         if(n>questions.length-1) return 0L;
 
@@ -8,11 +8,21 @@ class Solution {
         long ch2=(long)questions[n][0]+ans(n+questions[n][1]+1,questions,dp);
 
         return dp[n]=Math.max(ch1,ch2);
-    }
+    }*/
     public long mostPoints(int[][] questions) {
         int n=questions.length;
         long[] dp=new long[n];
         Arrays.fill(dp,-1L);
-        return ans(0,questions,dp);
+        
+        dp[n-1]=(long)questions[n-1][0];
+        for(int i=n-2;i>=0;i--){
+            long ch1=dp[i+1];
+            long ch2=questions[i][0];
+            if(i+questions[i][1]+1<n){
+                ch2+=dp[i+questions[i][1]+1];
+            }
+            dp[i]=Math.max(ch1,ch2);
+        }
+        return dp[0];
     }
 }
