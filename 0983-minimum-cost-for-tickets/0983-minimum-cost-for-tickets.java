987a36e2ee8c6164068e9857a96234d23eb01161
Ramm_Lakshmanan <rockingramm27@gmail.com>
2025-12-20 10:52:42 +0530

    Time: 2 ms (40.86%), Space: 43.2 MB (46.98%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int ans(int n,int[] days,int[] costs,int[] dp){
+    /*public int ans(int n,int[] days,int[] costs,int[] dp){
         if(n==0) return Math.min(Math.min(costs[0],costs[1]),costs[2]);
         if(n<0) return 0;
         if(dp[n]!=-1) return dp[n];
@@ -18,11 +18,29 @@ class Solution {
             else ch3=costs[2]+ans(Math.abs(search+2),days,costs,dp);
         }
         return dp[n]=Math.min(Math.min(ch1,ch2),ch3);
-    }
+    }*/
     public int mincostTickets(int[] days, int[] costs) {
         int n=days.length;
         int[] dp=new int[n];
         Arrays.fill(dp,-1);
-        return ans(n-1,days,costs,dp);
+        
+        dp[0]=Math.min(Math.min(costs[0],costs[1]),costs[2]);
+        for(int i=1;i<n;i++){
+            int ch1=costs[0]+dp[i-1];
+            int ch2=costs[1];
+            int ch3=costs[2];
+            if(days[i]-6>days[0]){
+                int search=Arrays.binarySearch(days,days[i]-6);
+                if(search>=0) ch2+=dp[search-1];
+                else ch2+=dp[Math.abs(search+2)]; 
+            }
+            if(days[i]-29>days[0]){
+                int search=Arrays.binarySearch(days,days[i]-29);
+                if(search>=0) ch3+=dp[search-1];
+                else ch3+=dp[Math.abs(search+2)]; 
+            }
+            dp[i]=Math.min(Math.min(ch1,ch2),ch3);
+        }
+        return dp[n-1];
     }
 }
