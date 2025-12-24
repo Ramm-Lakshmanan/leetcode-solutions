26e960d7edb22a8a8730c3887110c3aa72443d4e
Ramm_Lakshmanan <rockingramm27@gmail.com>
2025-12-24 10:54:27 +0530

    Time: 0 ms (100%), Space: 42.2 MB (19.82%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int ans(int row,int col,int[][] dp){
+        if(col==0 || row==0) return 1;
+        if(dp[row][col]!=-1) return dp[row][col];
+        return dp[row][col]=ans(row-1,col,dp)+ans(row,col-1,dp);
+    }
+    public int uniquePaths(int m, int n) {
+        int[][] dp=new int[m][n];
+        for(int i=0;i<m;i++) Arrays.fill(dp[i],-1);
+        return ans(m-1,n-1,dp);
+    }
+}
