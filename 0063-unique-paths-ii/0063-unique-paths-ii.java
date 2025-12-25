a2b87d099891be534a9c092d75ead43e31540e57
Ramm_Lakshmanan <rockingramm27@gmail.com>
2025-12-25 22:25:52 +0530

    Time: 0 ms (100%), Space: 43.8 MB (6.66%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int ans(int m,int n,int[][] obstacleGrid,int[][] dp){
+    /*public int ans(int m,int n,int[][] obstacleGrid,int[][] dp){
         if(obstacleGrid[m][n]==1){
             return 0;
         }
@@ -8,7 +8,7 @@ class Solution {
 
         if(dp[m][n]!=-1) return dp[m][n];
         return dp[m][n]=ans(m-1,n,obstacleGrid,dp)+ans(m,n-1,obstacleGrid,dp);
-    }
+    }*/
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m=obstacleGrid.length;
         int n=obstacleGrid[0].length;
@@ -24,6 +24,12 @@ class Solution {
             if(obstacleGrid[i][0]==1 || flag) {dp[i][0]=0; flag=true;}
             else dp[i][0]=1;
         }
-        return  ans(m-1,n-1,obstacleGrid,dp);
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(obstacleGrid[i][j]==1) dp[i][j]=0;
+                else dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
     }
 }
