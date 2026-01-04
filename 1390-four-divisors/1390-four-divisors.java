7903eacc11c5973ad0f21b2871c14f99df7118cf
Ramm_Lakshmanan <rockingramm27@gmail.com>
2026-01-04 11:55:27 +0530

    Time: 42 ms (23.23%), Space: 46.5 MB (40.91%) - LeetHub

---
+++ 1390-four-divisors/1390-four-divisors.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int sumFourDivisors(int[] nums) {
+        int res=0;
+        for(int i:nums){
+            int temp=0;
+            int cnt=0;
+            for(int j=1;j<=Math.sqrt(i);j++){
+                if(i%j==0){
+                    cnt++;
+                    temp+=j;
+                    if((i/j)!=j){
+                        cnt++;
+                        temp+=(i/j);
+                    }
+                }
+            }
+            if(cnt==4) res+=temp;
+        }
+        return res;
+    }
+}
