2afc9688714eb4f807fb4e7bc9c67334363404e8
Ramm_Lakshmanan <rockingramm27@gmail.com>
2026-01-19 10:47:54 +0530

    Time: 21 ms (13.71%), Space: 54.8 MB (86.29%) - LeetHub

--- 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.java
+++ 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.java
@@ -1,24 +1,30 @@
 class Solution {
-    public long func(int i,int j,int k,long[][] pref){
-        long sum=0L;
-        for(int x=i;x<=i+k;x++){
-            if(j==0)
-                sum+=(pref[x][j+k]);
-            else{
-                sum+=(pref[x][j+k]-pref[x][j-1]);
-            }
-        }
+    public long func(int i, int j, int k, long[][] pref2) {
+        int x2 = i + k, y2 = j + k;
+        long sum = pref2[x2][y2];
+        if (i > 0) sum -= pref2[i - 1][y2];
+        if (j > 0) sum -= pref2[x2][j - 1];
+        if (i > 0 && j > 0) sum += pref2[i - 1][j - 1];
         return sum;
     }
+
     public int maxSideLength(int[][] mat, int threshold) {
         int m=mat.length;
         int n=mat[0].length;
 
         long[][] pref=new long[m][n];
+        long[][] pref2=new long[m][n];
+
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(j==0) pref[i][j]=(long)mat[i][j];
                 else pref[i][j]=pref[i][j-1]+(long)mat[i][j];
+            }
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0) pref2[i][j]=pref[i][j];
+                else pref2[i][j]=pref2[i-1][j]+pref[i][j];
             }
         }
 
@@ -30,7 +36,7 @@ class Solution {
                 int low=0,high=req;
                 while(low<=high){
                     int mid=(low+high)/2;
-                    long comp=func(i,j,mid,pref);
+                    long comp=func(i,j,mid,pref2);
 
                     if(comp<=(long)threshold){
                         max=Math.max(max,mid+1);
