03af34d25aa2d7ca789d45f0c505506992e05133
Ramm_Lakshmanan <rockingramm27@gmail.com>
2026-02-11 10:17:01 +0530

    Time: 2 ms (66.97%), Space: 45 MB (32.02%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void func(List<String> res,int co,int cc,int n,String curr){
+        if(co==n && cc==n){
+            res.add(curr.toString());
+            return;
+        }
+        if(co>n || cc>n) return;
+
+        String temp1=curr;
+        temp1+="(";
+        func(res,co+1,cc,n,temp1);
+        if(co>0 && cc<co){
+            String temp2=curr;
+            temp2+=")";
+            func(res,co,cc+1,n,temp2);
+        }
+        
+    }
+    public List<String> generateParenthesis(int n) {
+        List<String> res=new ArrayList<>();
+        func(res,0,0,n,"");
+        return res;
+    }
+}
