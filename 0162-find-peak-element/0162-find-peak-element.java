3ffccbc6445f461d336b771ebb78923c92bf6eca
Ramm_Lakshmanan <rockingramm27@gmail.com>
2026-02-16 15:15:18 +0530

    Time: 0 ms (100%), Space: 44.3 MB (55.31%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -22,7 +22,7 @@ class Solution {
 
                 if(nums[mid]>nums[mid_l] && nums[mid]>nums[mid_g]) return mid;
                 else if(nums[mid_g]>nums[mid]) low=mid+1;
-                else if(nums[mid_l]>nums[mid]) high=mid-1;
+                else  high=mid-1;
             }
         }
         return 0;
