f3b33fb3d5954ac2e05fa82bf920cbadf6639cdf
Ramm_Lakshmanan <rockingramm27@gmail.com>
2026-02-23 14:31:00 +0530

    Time: 5 ms (10.07%), Space: 47.4 MB (5.85%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.java
@@ -0,0 +1,32 @@
+class Solution {
+    public void func(int[] candidates,int idx,int sum,int target,List<Integer> curr,Set<List<Integer>> ans){
+        
+        if(idx==-1 || sum>target) return;
+
+        List<Integer> temp_curr=new ArrayList<>();
+        temp_curr.addAll(curr);
+        int temp_sum=sum;
+        for(int i=0;i<(target-sum)/candidates[idx];i++){
+            temp_curr.add(candidates[idx]);
+            temp_sum+=candidates[idx];
+            if(temp_sum==target){
+                Collections.sort(temp_curr);
+                ans.add(temp_curr);
+            }
+            func(candidates,idx-1,temp_sum,target,temp_curr,ans);
+        }
+        if(sum==target){
+            Collections.sort(curr);
+            ans.add(curr);
+        }
+        func(candidates,idx-1,sum,target,curr,ans);
+    }
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        Set<List<Integer>> ans=new HashSet<>();
+        int n=candidates.length;
+        int idx=n-1,sum=0;
+        List<Integer> curr=new ArrayList<>();
+        func(candidates,idx,sum,target,curr,ans);
+        return new ArrayList<>(ans);
+    }
+}
