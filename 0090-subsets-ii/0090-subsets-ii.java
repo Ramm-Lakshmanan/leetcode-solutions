c995c54b3e2ed8aac624ac1d99512a7088cc7ac9
Ramm_Lakshmanan <rockingramm27@gmail.com>
2026-03-11 09:11:44 +0530

    Time: 2 ms (99.52%), Space: 45.2 MB (39.94%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,19 +1,21 @@
 class Solution {
-    public void func(Set<List<Integer>> ans,List<Integer> curr,int idx,int[] nums){
+    public void func(List<List<Integer>> ans,List<Integer> curr,int idx,int[] nums){
         if(idx==nums.length){
             ans.add(new ArrayList<>(curr));
             return;
         }
-        ans.add(new ArrayList<>(curr));
         curr.add(nums[idx]);
         func(ans,curr,idx+1,nums);
         curr.remove(curr.size()-1);
-        func(ans,curr,idx+1,nums);
+        
+        int next_idx=idx+1;
+        while(next_idx<nums.length && nums[next_idx]==nums[idx]) next_idx++;
+        func(ans,curr,next_idx,nums);
     }
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
-        Set<List<Integer>> ans=new HashSet<>();
+        List<List<Integer>> ans=new ArrayList<>();
         func(ans,new ArrayList<>(),0,nums);
-        return new ArrayList<>(ans);
+        return ans;
     }
 }
