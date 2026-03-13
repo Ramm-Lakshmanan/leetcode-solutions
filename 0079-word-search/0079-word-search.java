415a9db6157cb90177e930e91478cc1af747e564
Ramm_Lakshmanan <rockingramm27@gmail.com>
2026-03-13 12:02:23 +0530

    Time: 149 ms (41.11%), Space: 42.9 MB (86.31%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,49 +1,26 @@
 class Solution {
     public boolean func(int row,int col,int idx,String word,char[][] board,boolean[][] visited){
         if(idx==word.length()) return true;
-        if(row==board.length || col==board[0].length) return false;
-        if(board[row][col]!=word.charAt(idx)) {
-            if(idx==0){
-                if(col==board[0].length-1){
-                    return func(row+1,0,idx,word,board,visited);
-                }
-                else{
-                    return func(row,col+1,idx,word,board,visited);
-                }
-            }
-            else return false;
-        }
-        
-        if(idx==word.length()-1) return true;
+        if(row<0 || col<0 || row>=board.length || col>=board[0].length || visited[row][col]) return false;
+
+        if(board[row][col]!=word.charAt(idx)) return false;
         visited[row][col]=true;
-        boolean res=false;
-        if(!res && col!=board[0].length-1 && !visited[row][col+1]){
-            res=func(row,col+1,idx+1,word,board,visited);
-        }
-        if(!res && row!=board.length-1 && !visited[row+1][col]){
-            res=func(row+1,col,idx+1,word,board,visited);
-        }
-        if(!res && col!=0 && !visited[row][col-1]){
-            res=func(row,col-1,idx+1,word,board,visited);
-        }
-        if(!res && row!=0 && !visited[row-1][col]){
-            res=func(row-1,col,idx+1,word,board,visited);
-        }
-        if(!res)visited[row][col]=false;
-        if(!res && idx==0){
-            if(col==board[0].length-1){
-                return func(row+1,0,idx,word,board,visited);
-            }
-            else{
-                return func(row,col+1,idx,word,board,visited);
-            }
-        }
-        return res;
-}
+        boolean found= func(row-1,col,idx+1,word,board,visited)
+                || func(row+1,col,idx+1,word,board,visited)
+                || func(row,col-1,idx+1,word,board,visited)
+                || func(row,col+1,idx+1,word,board,visited);
+        visited[row][col]=false;
+        return found;
+    }
     public boolean exist(char[][] board, String word) {
-        int row=board.length,col=board[0].length;
-        boolean[][] visited=new boolean[row][col];
-        for(int i=0;i<row;i++) Arrays.fill(visited[i],false);
-        return func(0,0,0,word,board,visited);
+        int r=board.length,c=board[0].length;
+        boolean[][] visited=new boolean[r][c];
+        for(int i=0;i<r;i++) Arrays.fill(visited[i],false);
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(func(i,j,0,word,board,visited)) return true;
+            }
+        }
+        return false;
     }
 }
