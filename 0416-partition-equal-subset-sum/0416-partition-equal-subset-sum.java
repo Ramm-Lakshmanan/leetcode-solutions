8b78a9bc196a2689c3398b70338ea349fea0c021
Ramm_Lakshmanan <rockingramm27@gmail.com>
2026-04-11 14:38:32 +0530

    Time: 252 ms (5.03%), Space: 126.5 MB (5.04%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean func(int idx,int sum1,int sum2,int[] nums,Boolean[][] dp){
+        if(idx==-1){
+            if(sum1==sum2) return true;
+            else return false;
+        }
+
+        if(dp[idx][sum1]!=null) return dp[idx][sum1];
+
+        boolean ch1=func(idx-1,sum1+nums[idx],sum2,nums,dp);
+        boolean ch2=func(idx-1,sum1,sum2+nums[idx],nums,dp);
+
+        return dp[idx][sum1]=ch1 || ch2;
+    }
+    public boolean canPartition(int[] nums) {
+        int sum1=0,sum2=0,idx=nums.length-1;
+        int sum=0;
+        for(int i:nums) sum+=i;
+        Boolean[][] dp=new Boolean[nums.length][sum];
+        for(int i=0;i<nums.length;i++) Arrays.fill(dp[i],null);
+        return func(idx,sum1,sum2,nums,dp);
+    }
+}
