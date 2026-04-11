7e5c9fcceb0ce3d163fb59e546be900143e9c23a
Ramm_Lakshmanan <rockingramm27@gmail.com>
2026-04-11 09:19:45 +0530

    Time: 26 ms (28.9%), Space: 45.9 MB (98.07%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,14 +1,14 @@
 class Solution {
     public int func(int amount,int[] dp,int[] coins){
         if(amount==0) return 0;
-        else if(amount<0) return Integer.MIN_VALUE;
+        else if(amount<0) return -1;
 
-        if(dp[amount]!=Integer.MAX_VALUE) return dp[amount];
+        if(dp[amount]!=-2) return dp[amount];
 
         int min=Integer.MAX_VALUE;
         for(int coin:coins){
             int curr=func(amount-coin,dp,coins);
-            if(curr!=Integer.MIN_VALUE && curr!=-1) min=Math.min(min,1+curr);
+            if(curr>=0) min=Math.min(curr+1,min);
         }
 
         if(min==Integer.MAX_VALUE) return dp[amount]=-1;
@@ -16,7 +16,7 @@ class Solution {
     }
     public int coinChange(int[] coins, int amount) {
         int[] dp=new int[amount+1];
-        Arrays.fill(dp,Integer.MAX_VALUE);
+        Arrays.fill(dp,-2);
         return func(amount,dp,coins);
     }
 }
