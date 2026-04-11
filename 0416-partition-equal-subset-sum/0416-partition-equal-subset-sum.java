1839240d64b35e642ffc640be70e6242fa4b7054
Ramm_Lakshmanan <rockingramm27@gmail.com>
2026-04-11 14:41:21 +0530

    Time: 248 ms (5.03%), Space: 127.1 MB (5.04%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -16,7 +16,7 @@ class Solution {
         int sum1=0,sum2=0,idx=nums.length-1;
         int sum=0;
         for(int i:nums) sum+=i;
-        Boolean[][] dp=new Boolean[nums.length][sum];
+        Boolean[][] dp=new Boolean[nums.length][sum+1];
         for(int i=0;i<nums.length;i++) Arrays.fill(dp[i],null);
         return func(idx,sum1,sum2,nums,dp);
     }
