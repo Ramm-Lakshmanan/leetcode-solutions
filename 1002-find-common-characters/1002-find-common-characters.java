2941c9a0ba98641993887ee28e9961b430c33488
Ramm_Lakshmanan <rockingramm27@gmail.com>
2026-05-29 11:03:36 +0530

    Time: 19 ms (13.02%), Space: 45.6 MB (24.34%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.java
+++ 1002-find-common-characters/1002-find-common-characters.java
@@ -7,19 +7,14 @@ class Solution {
         }
 
         for(int j=1;j<words.length;j++){
-            Map<Character,Integer> hm1=new HashMap<>();
+            Map<Character,Integer> res=new HashMap<>();
             for(int i=0;i<words[j].length();i++){
-                hm1.put(words[j].charAt(i),hm1.getOrDefault(words[j].charAt(i),0)+1);
-            }
-
-            for(char c:hm.keySet()){
-                if(!hm1.containsKey(c)){
-                    hm.put(c,0);
-                }
-                else{
-                    hm.put(c,Math.min(hm1.get(c),hm.get(c)));
+                if(hm.getOrDefault(words[j].charAt(i),0)>0){
+                    res.put(words[j].charAt(i),res.getOrDefault(words[j].charAt(i),0)+1);
+                    hm.put(words[j].charAt(i),hm.getOrDefault(words[j].charAt(i),0)-1);
                 }
             }
+            hm=res;
         }
 
         for(char c:hm.keySet()){
