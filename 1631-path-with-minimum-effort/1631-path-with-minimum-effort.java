3799377e2a03046124f3fffc8b9d9261044624a8
Ramm_Lakshmanan <rockingramm27@gmail.com>
2026-06-10 13:50:43 +0530

    Time: 63 ms (18.53%), Space: 47.8 MB (72.31%) - LeetHub

---
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minimumEffortPath(int[][] heights) {
+        int row=heights.length,col=heights[0].length;
+        int[][] dist=new int[row][col];
+        for(int i=0;i<row;i++)Arrays.fill(dist[i],Integer.MAX_VALUE);
+
+        int[][] d={{1,0},{0,1},{-1,0},{0,-1}};
+
+        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->Integer.compare(a[0],b[0]));
+
+        pq.offer(new int[]{0,0,0});
+
+        while(!pq.isEmpty()){
+            int[] curr=pq.poll();
+            int cost=curr[0],x=curr[1],y=curr[2];
+
+            for(int i=0;i<4;i++){
+                if(x+d[i][0]>=0 && x+d[i][0]<row && y+d[i][1]>=0 && y+d[i][1]<col && Math.max(cost,Math.abs(heights[x][y]-heights[x+d[i][0]][y+d[i][1]]))<dist[x+d[i][0]][y+d[i][1]]){
+                    dist[x+d[i][0]][y+d[i][1]]=Math.max(cost,Math.abs(heights[x][y]-heights[x+d[i][0]][y+d[i][1]]));
+                    pq.offer(new int[]{dist[x+d[i][0]][y+d[i][1]],x+d[i][0],y+d[i][1]});
+                }
+            }
+        }
+        if(dist[row-1][col-1]==Integer.MAX_VALUE) return 0;
+        return dist[row-1][col-1];
+    }
+}
