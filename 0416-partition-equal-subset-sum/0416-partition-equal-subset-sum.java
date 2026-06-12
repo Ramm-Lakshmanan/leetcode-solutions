332300fbf7527f952473cfb2dbcaa8f03cc7a9f3
Ramm_Lakshmanan <rockingramm27@gmail.com>
2026-06-12 17:49:34 +0530

    Time: 46 ms (58.38%), Space: 47.6 MB (54.05%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,23 +1,20 @@
 class Solution {
-    public boolean func(int idx,int sum1,int sum2,int[] nums,Boolean[][] dp){
-        if(idx==-1){
-            if(sum1==sum2) return true;
-            else return false;
-        }
-
-        if(dp[idx][sum1]!=null) return dp[idx][sum1];
-
-        boolean ch1=func(idx-1,sum1+nums[idx],sum2,nums,dp);
-        boolean ch2=func(idx-1,sum1,sum2+nums[idx],nums,dp);
-
-        return dp[idx][sum1]=ch1 || ch2;
-    }
     public boolean canPartition(int[] nums) {
-        int sum1=0,sum2=0,idx=nums.length-1;
-        int sum=0;
-        for(int i:nums) sum+=i;
-        Boolean[][] dp=new Boolean[nums.length][sum+1];
-        for(int i=0;i<nums.length;i++) Arrays.fill(dp[i],null);
-        return func(idx,sum1,sum2,nums,dp);
+        int sum=0,n=nums.length;
+        for(int ele:nums) sum+=ele;
+
+        if(sum%2!=0) return false;
+
+        boolean[][] dp=new boolean[n+1][(sum/2)+1];
+        
+        for(int i=0;i<=n;i++) dp[i][0]=true;
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=(sum/2);j++){
+                if(nums[i-1]>j) dp[i][j]=dp[i-1][j];
+                else dp[i][j]=dp[i-1][j] | dp[i-1][j-nums[i-1]];
+            }
+        }
+        return dp[n][(sum/2)];
     }
 }
