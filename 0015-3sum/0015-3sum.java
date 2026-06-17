d212069c1b76b3e9d5377951c1ad55943983d5e0
Ramm_Lakshmanan <rockingramm27@gmail.com>
2026-06-17 12:38:58 +0530

    Time: 32 ms (78.97%), Space: 60.5 MB (7.63%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,35 +1,38 @@
-import java.util.*;
-
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> l = new ArrayList<>();
-        Arrays.sort(nums);  // Sort the array first
+        Arrays.sort(nums);
+        int n=nums.length;
 
-        for (int i = 0; i < nums.length - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;  // Skip duplicates for i
+        List<List<Integer>> ans=new ArrayList<>();
+        int i=0;
+        while(i<n-2){
 
-            int j = i + 1;
-            int k = nums.length - 1;
+            int j=i+1,k=n-1;
+            
 
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-
-                if (sum < 0) {
-                    j++;
-                } else if (sum > 0) {
+            while(j<k){
+                int sum=nums[i]+nums[k]+nums[j];
+                if(sum>0){
                     k--;
-                } else {
-                    l.add(Arrays.asList(nums[i], nums[j], nums[k]));
-
-                    // Skip duplicates for j and k
-                    while (j < k && nums[j] == nums[j + 1]) j++;
-                    while (j < k && nums[k] == nums[k - 1]) k--;
-
+                }
+                else if(sum<0){
+                    j++;
+                }
+                else{
+                    while(j<k && nums[j]==nums[j+1]) j++;
+                    while(j<k && nums[k]==nums[k-1]) k--;
+                    List<Integer> temp=new ArrayList<>();
+                    temp.add(nums[i]);
+                    temp.add(nums[j]);
+                    temp.add(nums[k]);
+                    ans.add(temp);
                     j++;
                     k--;
                 }
             }
+            while(i<n-2 && nums[i]==nums[i+1]) i++;
+            i++;
         }
-        return l;
+        return ans;
     }
 }
