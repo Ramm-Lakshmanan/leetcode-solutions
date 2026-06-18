6ae9b4620c69ebc855c57e84a9c7ad96fb8af333
Ramm_Lakshmanan <rockingramm27@gmail.com>
2026-06-18 20:17:21 +0530

    Time: 13 ms (91.46%), Space: 46.4 MB (59.39%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,23 +1,34 @@
 class Solution {
-    public int func(int amount,int[] coins,int[] dp){
-        if(amount<0) return Integer.MAX_VALUE;
-        if(amount==0) return 0;
+    // public int func(int amount,int[] coins,int[] dp){
+    //     if(amount<0) return Integer.MAX_VALUE;
+    //     if(amount==0) return 0;
 
-        if(dp[amount]!=-1) return dp[amount];
+    //     if(dp[amount]!=-1) return dp[amount];
 
-        int ans=Integer.MAX_VALUE;
+    //     int ans=Integer.MAX_VALUE;
 
-        for(int coin:coins){
-            int res=func(amount-coin,coins,dp);
-            if(res!=Integer.MAX_VALUE) ans=Math.min(ans,1+res);
-        }
-        return dp[amount]=ans;
-    }
+    //     for(int coin:coins){
+    //         int res=func(amount-coin,coins,dp);
+    //         if(res!=Integer.MAX_VALUE) ans=Math.min(ans,1+res);
+    //     }
+    //     return dp[amount]=ans;
+    // }
     public int coinChange(int[] coins, int amount) {
         int[] dp=new int[amount+1];
-        Arrays.fill(dp,-1);
-        int res= func(amount,coins,dp);
-        if(res==Integer.MAX_VALUE) return -1;
-        return res;
+        Arrays.fill(dp,Integer.MAX_VALUE);
+        dp[0]=0;
+
+        for(int i=1;i<=amount;i++){
+            int res=Integer.MAX_VALUE;
+            for(int coin:coins){
+                if(i-coin>=0 && dp[i-coin]!=Integer.MAX_VALUE){
+                    res=Math.min(res,1+dp[i-coin]);
+                }
+            }
+            dp[i]=res;
+
+        }
+        if(dp[amount]==Integer.MAX_VALUE) return -1;
+        return dp[amount];
     }
 }
