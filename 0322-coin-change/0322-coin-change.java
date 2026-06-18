c7a3c7bde86937d7c8833baa3ab499d4084a0029
Ramm_Lakshmanan <rockingramm27@gmail.com>
2026-06-18 20:18:34 +0530

    Time: 13 ms (91.46%), Space: 45.9 MB (97.7%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -15,8 +15,6 @@ class Solution {
     // }
     public int coinChange(int[] coins, int amount) {
         int[] dp=new int[amount+1];
-        Arrays.fill(dp,Integer.MAX_VALUE);
-        dp[0]=0;
 
         for(int i=1;i<=amount;i++){
             int res=Integer.MAX_VALUE;
