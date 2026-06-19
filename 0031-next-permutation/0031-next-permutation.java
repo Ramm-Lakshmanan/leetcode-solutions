37c6da0b6062c216876003d54d8e6a5a0c2bb08e
Ramm_Lakshmanan <rockingramm27@gmail.com>
2026-06-19 12:23:08 +0530

    Time: 0 ms (100%), Space: 44.5 MB (82.68%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,40 +1,37 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int idx=-1;
         int n=nums.length;
-
-        for(int i=n-1;i>=1;i--){
-            if(nums[i]>nums[i-1]){
-                idx=i-1;
+        int idx=-1;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                idx=i;
                 break;
             }
         }
         if(idx==-1){
-            for(int i=0;i<(n/2);i++){
+            for(int i=0;i<n/2;i++){
                 int temp=nums[i];
                 nums[i]=nums[n-i-1];
                 nums[n-i-1]=temp;
             }
-            return ;
+            return;
         }
-        
-        for(int i=n-1;i>idx;i--){
+        for(int i=n-1;i>=0;i--){
             if(nums[i]>nums[idx]){
-                int temp=nums[idx];
-                nums[idx]=nums[i];
-                nums[i]=temp;
+                int temp=nums[i];
+                nums[i]=nums[idx];
+                nums[idx]=temp;
                 break;
             }
         }
-        int left=idx+1;
-        int right=n-1;
+        int left=idx+1,right=n-1;
+
         while(left<right){
-            int temp2=nums[left];
+            int temp=nums[left];
             nums[left]=nums[right];
-            nums[right]=temp2;
+            nums[right]=temp;
             left++;
             right--;
         }
-        return;
     }
 }
