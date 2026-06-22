78f1ec18068ddb149c9eb24c6053beab538e414c
Ramm_Lakshmanan <rockingramm27@gmail.com>
2026-06-22 21:54:26 +0530

    Time: 13 ms (42.41%), Space: 47 MB (60.85%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -4,9 +4,12 @@ class Solution {
         int[] dp=new int[amount+1];
         dp[0]=1;
         for(int i=1;i<=n;i++){
+            int[] temp=new int[amount+1];
             for(int j=0;j<=amount;j++){
-                if(j-coins[i-1]>=0) dp[j]+=dp[j-coins[i-1]];
+                temp[j]+=dp[j];
+                if(j-coins[i-1]>=0) temp[j]+=temp[j-coins[i-1]];
             }
+            dp=temp;
         }
         return dp[amount];
     }
