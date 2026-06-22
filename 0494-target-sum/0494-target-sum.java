efe97caf9b72248c6db3425bbd6af8440acf45ec
Ramm_Lakshmanan <rockingramm27@gmail.com>
2026-06-22 11:45:11 +0530

    Time: 8 ms (60.66%), Space: 46.5 MB (30.57%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int func(int idx,int cur,int target,int[] nums,int[][] dp,int sum){
+        if(idx==-1){
+            if(target==cur) return 1;
+            return 0;
+        }
+
+        if(dp[idx][sum+cur]!=-1) return dp[idx][sum+cur];
+        int c=0;
+        c+=func(idx-1,cur-nums[idx],target,nums,dp,sum);
+        c+=func(idx-1,cur+nums[idx],target,nums,dp,sum);
+        return dp[idx][sum+cur]=c;
+    }
+    public int findTargetSumWays(int[] nums, int target) {
+        int n=nums.length,sum=0;
+        for(int ele:nums) sum+=ele;
+
+        int[][] dp=new int[n][(2*sum)+1];
+        for(int i=0;i<n;i++) Arrays.fill(dp[i],-1);
+
+        return func(n-1,0,target,nums,dp,sum);
+    }
+}
