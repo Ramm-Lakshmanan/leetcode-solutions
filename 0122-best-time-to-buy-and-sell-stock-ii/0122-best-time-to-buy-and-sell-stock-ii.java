ad4ffbd492f6afbc44c878048e982b9b7106a4a4
Ramm_Lakshmanan <rockingramm27@gmail.com>
2026-06-24 23:07:49 +0530

    Time: 0 ms (100%), Space: 46.1 MB (95.97%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,24 +1,31 @@
 class Solution {
-    public int func(int idx,int max,int[] prices,int[] dp){
-        if(idx==-1) return 0;
+    // public int func(int idx,int max,int[] prices,int[] dp){
+    //     if(idx==-1) return 0;
 
-        if(dp[idx]!=-1) return dp[idx];
+    //     if(dp[idx]!=-1) return dp[idx];
 
-        max=Math.max(max,prices[idx]);
-        int ch1=0,ch2=0;
-        if(max>prices[idx]){
-            ch1=max-prices[idx]+func(idx-1,prices[idx],prices,dp);
-        }
-        ch2=func(idx-1,max,prices,dp);
+    //     max=Math.max(max,prices[idx]);
+    //     int ch1=0,ch2=0;
+    //     if(max>prices[idx]){
+    //         ch1=max-prices[idx]+func(idx-1,prices[idx],prices,dp);
+    //     }
+    //     ch2=func(idx-1,max,prices,dp);
 
-        return dp[idx]=Math.max(ch1,ch2);
-    }
+    //     return dp[idx]=Math.max(ch1,ch2);
+    // }
     public int maxProfit(int[] prices) {
         int n=prices.length;
 
-        int[] dp=new int[n];
-        Arrays.fill(dp,-1);
+        int profit=0;
+        int max=prices[n-1];
 
-        return func(n-1,0,prices,dp);
+        for(int i=n-2;i>=0;i--){
+            if(prices[i]>max) max=prices[i];
+            else{
+                profit+=(max-prices[i]);
+                max=prices[i];
+            }
+        }
+        return profit;
     }
 }
