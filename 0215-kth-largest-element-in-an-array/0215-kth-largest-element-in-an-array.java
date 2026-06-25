7377f7963b08f39ce2832e6ad86367900a14bd85
Ramm_Lakshmanan <rockingramm27@gmail.com>
2026-06-25 22:58:52 +0530

    Time: 156 ms (5.17%), Space: 72.8 MB (32.3%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,42 +1,46 @@
 class Solution {
-    public int partition(int l,int r,int[] nums){
-        int i=l-1;
-        int pivot=nums[r];
+    // public int partition(int l,int r,int[] nums){
+    //     int i=l-1;
+    //     int pivot=nums[r];
 
-        for(int j=l;j<r;j++){
-            if(nums[j]>pivot){
-                i++;
-                int temp=nums[j];
-                nums[j]=nums[i];
-                nums[i]=temp;
-            }
-        }
-        i++;
-        int temp=nums[i];
-        nums[i]=nums[r];
-        nums[r]=temp;
+    //     for(int j=l;j<r;j++){
+    //         if(nums[j]>pivot){
+    //             i++;
+    //             int temp=nums[j];
+    //             nums[j]=nums[i];
+    //             nums[i]=temp;
+    //         }
+    //     }
+    //     i++;
+    //     int temp=nums[i];
+    //     nums[i]=nums[r];
+    //     nums[r]=temp;
 
-        return i;
-    }
-    public void qsort(int l,int r,int[] nums,int k){
+    //     return i;
+    // }
+    // public void qsort(int l,int r,int[] nums,int k){
         
-        if(l<=r){
-            int q=partition(l,r,nums);
+    //     if(l<=r){
+    //         int q=partition(l,r,nums);
 
-            if(q+1==k){
-                return;
-            }
-            else if(q+1<k){
-                qsort(q+1,r,nums,k);
-            }
-            else{
-                qsort(l,q-1,nums,k);
-            }
-        }
-    }
+    //         if(q+1==k){
+    //             return;
+    //         }
+    //         else if(q+1<k){
+    //             qsort(q+1,r,nums,k);
+    //         }
+    //         else{
+    //             qsort(l,q-1,nums,k);
+    //         }
+    //     }
+    // }
     public int findKthLargest(int[] nums, int k) {
-        int n=nums.length;
-        qsort(0,n-1,nums,k);
-        return nums[k-1];
+        PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
+        for(int ele:nums) pq.offer(ele);
+        while(k>1){
+            pq.poll();
+            k--;
+        }
+        return pq.peek();
     }
 }
