a12ef556c49546c3c4b77852ca88817895c27102
Ramm_Lakshmanan <rockingramm27@gmail.com>
2026-06-25 23:16:03 +0530

    Time: 2254 ms (5.17%), Space: 76.3 MB (5.79%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,42 +1,54 @@
-class Solution {
-    public int partition(int l,int r,int[] nums){
-        int i=l-1;
-        int pivot=nums[r];
+import java.util.Random;
 
-        for(int j=l;j<r;j++){
-            if(nums[j]>pivot){
+class Solution {
+    private final Random rand = new Random(); // Reuse Random instance
+
+    public int partition(int l, int r, int[] nums) {
+        // 1. Pick a random index between l and r
+        int randomIndex = l + rand.nextInt(r - l + 1);
+        
+        // 2. Swap it to the end so your existing logic works perfectly
+        int pTemp = nums[randomIndex];
+        nums[randomIndex] = nums[r];
+        nums[r] = pTemp;
+
+        int i = l - 1;
+        int pivot = nums[r];
+
+        for (int j = l; j < r; j++) {
+            if (nums[j] > pivot) { // Descending order
                 i++;
-                int temp=nums[j];
-                nums[j]=nums[i];
-                nums[i]=temp;
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
             }
         }
         i++;
-        int temp=nums[i];
-        nums[i]=nums[r];
-        nums[r]=temp;
+        int temp = nums[i];
+        nums[i] = nums[r];
+        nums[r] = temp;
 
         return i;
     }
-    public int qsort(int l,int r,int[] nums,int k){
-        
-        while(l<=r){
-            int q=partition(l,r,nums);
 
-            if(q+1==k){
+    public int qsort(int l, int r, int[] nums, int k) {
+        while (l <= r) {
+            int q = partition(l, r, nums);
+
+            if (q + 1 == k) {
                 return nums[q];
             }
-            else if(q+1<k){
-                l=q+1;
+            else if (q + 1 < k) {
+                l = q + 1;
             }
-            else{
-                r=q-1;
+            else {
+                r = q - 1;
             }
         }
-        return nums[k-1];
+        return nums[k - 1];
     }
+
     public int findKthLargest(int[] nums, int k) {
-        int n=nums.length;
-        return qsort(0,n-1,nums,k);
+        return qsort(0, nums.length - 1, nums, k);
     }
 }
