df221f3d1dc0bbffa7fcf4efd5193dad4947397e
Ramm_Lakshmanan <rockingramm27@gmail.com>
2026-06-30 18:35:17 +0530

    Time: 2 ms (88.73%), Space: 47.8 MB (80.28%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -2,21 +2,13 @@ class Solution {
     public boolean canJump(int[] nums) {
         int n=nums.length;
         if(nums[0]>=n-1) return true;
-
-        PriorityQueue<Integer> pq=new PriorityQueue<>();
         int targ=n-1;
-        for(int i=0;i<n;i++){
-            if(nums[i]>=(targ-i)) pq.offer(i);
-        }
-
         int k=nums[0];   
         int i=1;
         while(k>0  && i<n){
             k--;
-            while(!pq.isEmpty() && i>pq.peek()) pq.poll();
-            if(pq.isEmpty()) return false;
 
-            if(nums[i]>=pq.peek()-i) return true;
+            if(nums[i]>=targ-i) return true;
             k=Math.max(k,nums[i]);
             i++;
         }   
