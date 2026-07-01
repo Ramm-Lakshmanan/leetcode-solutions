789c1a0a5d8d8e0273ffa2a07d074001f5b33e02
Ramm_Lakshmanan <rockingramm27@gmail.com>
2026-07-02 00:39:09 +0530

    Time: 207 ms (5.17%), Space: 65.7 MB (100%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
@@ -1,29 +1,24 @@
 class Solution {
     public int[] arrayRankTransform(int[] arr) {
-        int n=arr.length;
-        if(n==0) return arr;
-
-        int[] ans=new int[n];
-        
-        int[][] pq=new int[n][2];
-
-        for(int i=0;i<n;i++){
-            pq[i][0]=arr[i];
-            pq[i][1]=i;
+        if (arr.length == 0) return arr;
+        int[][] pq = new int[arr.length][2];
+        for (int i = 0; i < arr.length; i++) {
+            pq[i][0] = arr[i];
+            pq[i][1] = i;
         }
-
-        Arrays.sort(pq,(a,b)->Integer.compare(a[0],b[0]));
-        int rank=1;
-        ans[pq[0][1]]=rank;
-        int prev=pq[0][0];
-
-        for(int i=1;i<n;i++){
-            if(prev!=pq[i][0]){
+        java.util.Arrays.sort(pq, (a, b) -> Integer.compare(a[0], b[0]));
+        int rank = 1;
+        int prevValue = pq[0][0];
+        arr[pq[0][1]] = rank;
+        for (int i = 1; i < pq.length; i++) {
+            if (pq[i][0] == prevValue) {
+                arr[pq[i][1]] = rank;
+            } else {
                 rank++;
-                prev=pq[i][0];
+                prevValue = pq[i][0];
+                arr[pq[i][1]] = rank;
             }
-            ans[pq[i][1]]=rank;
         }
-        return ans;
+        return arr;
     }
 }
