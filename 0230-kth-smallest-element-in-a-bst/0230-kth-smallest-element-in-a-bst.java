93ac8a0f18f39db2541d3314d40bfce19b7b03db
Ramm_Lakshmanan <rockingramm27@gmail.com>
2026-07-02 19:27:07 +0530

    Time: 0 ms (100%), Space: 46.6 MB (68.62%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -14,27 +14,25 @@
  * }
  */
 class Solution {
-
-    public int[] find(TreeNode root,int k,int cnt,int res){
-        int data=root.val;
-
+    public int[] func(TreeNode root,int k){
+        
         if(root.left!=null){
-            int[] ans=find(root.left,k,cnt,res);
-            cnt=ans[1];
-            res=ans[0];
+            int[] res=func(root.left,k);
+            if(res[0]==0) return res;
+            k=res[0];
         }
-        cnt=cnt+1;
-        if(cnt==k){
-            return new int[]{data,cnt};
-        }
+        k--;
+        if(k==0) return new int[]{k,root.val};
+
         if(root.right!=null){
-            int[] ans=find(root.right,k,cnt,res);
-            cnt=ans[1];
-            res=ans[0];
+            int[] res=func(root.right,k);
+            if(res[0]==0) return res;
+            k=res[0];
         }
-        return new int[]{res,cnt};
+        return new int[]{k,-1};
+
     }
     public int kthSmallest(TreeNode root, int k) {
-        return find(root,k,0,-1)[0];
+        return func(root,k)[1];
     }
 }
