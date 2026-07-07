85cd811b826a0ef9b0650302c90930aa5a3f71e1
Ramm_Lakshmanan <rockingramm27@gmail.com>
2026-07-07 09:41:12 +0530

    Time: 0 ms (100%), Space: 52.4 MB (97.23%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -9,13 +9,14 @@ class Solution {
 
             if(mid%2!=0){
                 if(nums[mid]==nums[mid-1]) low=mid+1;
-                else high=mid-1;
+                else high=mid;
             }
             else{
-                if(mid>0 && nums[mid]==nums[mid-1]) high=mid-1;
+                if(nums[mid]==nums[mid+1]) {
+                    low=mid+1;
+                }
                 else{
-                    if(mid==n-1 || nums[mid]!=nums[mid+1]) return nums[mid];
-                    else low=mid+1;
+                    high=mid;
                 }
             }
         }
