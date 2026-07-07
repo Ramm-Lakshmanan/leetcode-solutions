1c35ab722a97587b1f70b9f0428e99edae51d4d1
Ramm_Lakshmanan <rockingramm27@gmail.com>
2026-07-07 19:32:45 +0530

    Time: 17 ms (72.41%), Space: 64.3 MB (6.3%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,27 +1,25 @@
 class Solution {
-    public boolean isMatch(String s, String p) {
-        int i = 0, j = 0;
-        int starIndex = -1;
-        int matchIndex = 0;
-        while (i < s.length()) {
-            if (j < p.length() && (p.charAt(j) == s.charAt(i) || p.charAt(j) == '?')) {
-                i++;
-                j++;
-            } else if (j < p.length() && p.charAt(j) == '*') {
-                starIndex = j;
-                matchIndex = i;
-                j++;
-            } else if (starIndex != -1) {
-                j = starIndex + 1;
-                matchIndex++;
-                i = matchIndex;
-            } else {
-                return false;
+    public boolean func(int i,int j,String s,String p,Boolean[][] dp){
+        if(i==-1 && j==-1)  return true;
+        else if(j==-1) return false;
+        else if(i==-1){
+            for(int k=j;k>=0;k--){
+                if(p.charAt(k)!='*') return false;
             }
+            return true;
         }
-        while (j < p.length() && p.charAt(j) == '*') {
-            j++;
+
+        if(dp[i][j]!=null) return dp[i][j];
+
+        if(p.charAt(j)=='?') return dp[i][j]=func(i-1,j-1,s,p,dp);
+        else if(p.charAt(j)=='*'){
+            return dp[i][j]=func(i,j-1,s,p,dp) || func(i-1,j,s,p,dp);
         }
-        return j == p.length();
+        else return dp[i][j]=(s.charAt(i)==p.charAt(j)) && func(i-1,j-1,s,p,dp);
+    }
+    public boolean isMatch(String s, String p) {
+        int n=s.length(),m=p.length();
+        Boolean[][] dp=new Boolean[n][m];
+        return func(n-1,m-1,s,p,dp);
     }
 }
