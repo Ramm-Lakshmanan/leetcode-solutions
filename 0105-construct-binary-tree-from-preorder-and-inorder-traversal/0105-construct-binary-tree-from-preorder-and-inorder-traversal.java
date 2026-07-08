189fc3ca8cca2f00d41d381a526a82e25a985877
Ramm_Lakshmanan <rockingramm27@gmail.com>
2026-07-08 10:59:33 +0530

    Time: 172 ms (6.07%), Space: 47.3 MB (7.14%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -14,33 +14,35 @@
  * }
  */
 class Solution {
+    public TreeNode func(TreeNode root,int val,Map<Integer,Integer> hm){
+        if(root==null){
+            root=new TreeNode(val,null,null);
+        }
+
+        else if(hm.get(val)<hm.get(root.val)){
+            root.left=func(root.left,val,hm);
+        }
+
+        else{
+            root.right=func(root.right,val,hm);
+        }
+
+        return root;
+    }
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        int n=inorder.length;
+        int n=preorder.length;
         Map<Integer,Integer> hm=new HashMap<>();
+
         for(int i=0;i<n;i++){
             hm.put(inorder[i],i);
         }
-        Deque<TreeNode> stk=new ArrayDeque<>();
-        TreeNode root=new TreeNode(preorder[0],null,null);
-        stk.offerLast(root);
-        for(int i:preorder){
-            if(i!=preorder[0]){
-                TreeNode nn=new TreeNode(i,null,null);
-                if(stk.isEmpty() || hm.get(stk.peekLast().val)>hm.get(i)){
-                    if(!stk.isEmpty()){
-                        stk.peekLast().left=nn;
-                    }
-                }
-                else{
-                    TreeNode temp=root;
-                    while(!stk.isEmpty() && hm.get(stk.peekLast().val)<hm.get(i)){
-                        temp=stk.pollLast();
-                    }
-                    temp.right=nn;
-                }
-                stk.offerLast(nn);
-            }   
+
+        TreeNode root=func(null,preorder[0],hm);
+
+        for(int i=1;i<n;i++){
+            root=func(root,preorder[i],hm);
         }
+
         return root;
     }
 }
