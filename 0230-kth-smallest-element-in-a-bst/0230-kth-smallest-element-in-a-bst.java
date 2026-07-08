548f0d46e7203755bf1ebd68e26aca767b269814
Ramm_Lakshmanan <rockingramm27@gmail.com>
2026-07-08 09:49:05 +0530

    Time: 0 ms (100%), Space: 46.8 MB (50.57%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -14,21 +14,21 @@
  * }
  */
 class Solution {
-    int res=0,cnt=0;
+    int cnt=0;
+    int ans=-1;
+    public void func(TreeNode root,int k){
+        if(root==null)  return;
 
-    public void inorder(TreeNode root,int k){
-        if(root==null) return;
-        inorder(root.left,k);
+        func(root.left,k);
         cnt++;
-
         if(cnt==k){
-            res=root.val;
+            ans=root.val;
             return;
-        }
-        inorder(root.right,k);
+        } 
+        func(root.right,k);  
     }
     public int kthSmallest(TreeNode root, int k) {
-        inorder(root,k);
-        return res;
+        func(root,k);
+        return ans;
     }
 }
