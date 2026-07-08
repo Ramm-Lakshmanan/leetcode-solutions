a747cfc2cb9da6a1b01674757f06e17ddfad197b
Ramm_Lakshmanan <rockingramm27@gmail.com>
2026-07-08 20:56:34 +0530

    Time: 35 ms (5.58%), Space: 50.9 MB (8.28%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -7,19 +7,19 @@ class Solution {
             for(int j=0;j<strs[i].length();j++){
                 req.put(strs[i].charAt(j),req.getOrDefault(strs[i].charAt(j),0)+1);
             }
-            String entry="";
+            StringBuilder entry=new StringBuilder();
             for(char c:req.keySet()){
-                entry+=(Character.toString(c)+Integer.toString(req.get(c)));
+                entry.append(Character.toString(c)+Integer.toString(req.get(c)));
             }
 
-            if(hm.containsKey(entry)){
-                List<String> temp=hm.get(entry);
+            if(hm.containsKey(entry.toString())){
+                List<String> temp=hm.get(entry.toString());
                 temp.add(strs[i]);
             }
             else{
                 List<String> temp=new ArrayList<>();
                 temp.add(strs[i]);
-                hm.put(entry,temp);
+                hm.put(entry.toString(),temp);
             }
         }
 
