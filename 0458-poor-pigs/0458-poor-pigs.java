cfd421a27d5553f54015ec5ffc1a8cbd263b68a7
Ramm_Lakshmanan <rockingramm27@gmail.com>
2026-07-08 15:22:22 +0530

    Time: 0 ms (100%), Space: 42.1 MB (50.14%) - LeetHub

--- 0458-poor-pigs/0458-poor-pigs.java
+++ 0458-poor-pigs/0458-poor-pigs.java
@@ -1,13 +1,6 @@
 class Solution {
     public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
-        if(buckets==1) return 0;
-        int rounds=(minutesToTest/minutesToDie)+1;
-        int ans=rounds;
-        int cnt=1;
-        while(ans<buckets) {
-            cnt++;
-            ans*=rounds;
-        }
-        return cnt;
+        int round = minutesToTest / minutesToDie + 1;
+        return (int) Math.ceil(Math.log10(buckets) / Math.log10(round));
     }
 }
