e15f5dca5d1e9bd22e5c673c0e949248ad7d56f8
Ramm_Lakshmanan <rockingramm27@gmail.com>
2026-07-09 11:20:49 +0530

    Time: 6 ms (38.17%), Space: 44.9 MB (42.94%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -5,11 +5,11 @@ class Solution {
         StringBuilder nn=new StringBuilder();
 
         for(int i=0;i<s.length();i++){
-            if(Character.isDigit(s.charAt(i)) || (s.charAt(i)>='a' && s.charAt(i)<='z')) nn.append(s.charAt(i));
+            if(Character.isDigit(s.charAt(i)) || Character.isLetter(s.charAt(i))) nn.append(s.charAt(i));
         }
-
-        for(int i=0;i<nn.length()/2;i++){
-            if(nn.charAt(i)!=nn.charAt(nn.length()-i-1)) return false;
+        int n=nn.length();
+        for(int i=0;i<n/2;i++){
+            if(nn.charAt(i)!=nn.charAt(n-i-1)) return false;
         }
         return true;
     }
