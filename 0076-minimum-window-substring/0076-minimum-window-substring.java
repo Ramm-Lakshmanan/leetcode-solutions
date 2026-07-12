2be3dfba7593571db7480ae983e4d1eaefe8ddd3
Ramm_Lakshmanan <rockingramm27@gmail.com>
2026-07-13 00:33:23 +0530

    Time: 15 ms (59.7%), Space: 47.5 MB (33.43%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,45 +1,44 @@
 class Solution {
-    /*public int check( Map<Character,Integer> hm){
-        int ans=0;
-        for(char c:hm.keySet()){
-            if(hm.get(c)>0){
-                ans=1;
-                break;
-            }
-            else if(hm.get(c)<0){
-                ans=-1;
-            }
-        }
-        return ans;
-    }*/
     public String minWindow(String s, String t) {
-        if(s.equals(t)) return s;
         int m=s.length(),n=t.length();
-        int req=0;
-        Map<Character,Integer> hm=new HashMap<>();
-        for(int i=0;i<n;i++) hm.put(t.charAt(i),hm.getOrDefault(t.charAt(i),0)+1);
+        if(m<n) return "";
 
-        int left=0,right=0,missing=t.length();
+        Map<Character,Integer> hm=new HashMap<>();
+        for(int i=0;i<n;i++){
+            hm.put(t.charAt(i),hm.getOrDefault(t.charAt(i),0)+1);
+        }
+
+        int rem=n,left=0,right=0;
+        int[] ans=new int[2];
         int min=Integer.MAX_VALUE;
-        String res="";
 
         while(right<m){
-            char c=s.charAt(right);
-            if(hm.getOrDefault(c,0)>0) missing--;
-
-            hm.put(s.charAt(right),hm.getOrDefault(s.charAt(right),0)-1);
-
-            while(missing==0){
-                if(right-left+1<min){
-                    min=right-left+1;
-                    res=s.substring(left,right+1);
-                }
-                hm.put(s.charAt(left),hm.getOrDefault(s.charAt(left),0)+1);
-                if(hm.getOrDefault(s.charAt(left),0)>0) missing++;
-                left++;
+            if(hm.containsKey(s.charAt(right))){
+                int occr=hm.get(s.charAt(right));
+                if(occr>0) rem--;
+                hm.put(s.charAt(right),occr-1);
             }
+
+            while(rem==0){
+                if(right-left+1<min){
+                    ans[0]=left;ans[1]=right;
+                    min=right-left+1;
+                }
+
+                if(!hm.containsKey(s.charAt(left))){
+                    left++;
+                }
+                else{
+                    int occr=hm.get(s.charAt(left));
+                    if(occr==0) rem++;
+                    hm.put(s.charAt(left),occr+1);
+                    left++;
+                }
+            }
+
             right++;
         }
-        return res;
+
+        return min==Integer.MAX_VALUE?"":s.substring(ans[0],ans[1]+1);
     }
 }
