1736f567fc94fd61a1fc72f7e474b675b29c01a6
Ramm_Lakshmanan <rockingramm27@gmail.com>
2026-07-17 21:56:52 +0530

    Time: 0 ms (100%), Space: 43 MB (22.94%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,29 +1,25 @@
 class Solution {
+    public int func(int cur,int end,int[] nums,int[] dp){
+        if(cur<end) return 0;
+
+        if(dp[cur]!=-1) return dp[cur];
+
+        int take=nums[cur]+func(cur-2,end,nums,dp);
+        int ntake=func(cur-1,end,nums,dp);
+
+        return dp[cur]=Math.max(take,ntake);
+    }
     public int rob(int[] nums) {
         int n=nums.length;
-        int max=0;
-        int prev1=nums[0],prev2=0,curr=-1;
-        for(int i=1;i<n-1;i++){
-            int ch1=prev1;
-            int ch2=nums[i];
-            if(i>=2) ch2+=prev2;
-            curr=Math.max(ch1,ch2);
-            prev2=prev1;
-            prev1=curr;
-        }
-        max=prev1;
+        if(n==1) return nums[0];
+        int[] dp1=new int[n];
+        int[] dp2=new int[n];
 
-        if(n>1) prev1=nums[1];
-        prev2=0;
-        curr=-1;
-        for(int i=2;i<n;i++){
-            int ch1=prev1;
-            int ch2=nums[i];
-            if(i>=2) ch2+=prev2;
-            curr=Math.max(ch1,ch2);
-            prev2=prev1;
-            prev1=curr;
-        }
-        return Math.max(max,prev1);
+        Arrays.fill(dp1,-1);Arrays.fill(dp2,-1);
+
+        int res1=func(n-1,1,nums,dp1);
+        int res2=func(n-2,0,nums,dp2);
+
+        return Math.max(res1,res2);
     }
 }
