39412a904c4262ef11d8ec9d5175a355665ffe5e
Ramm_Lakshmanan <rockingramm27@gmail.com>
2026-07-21 18:43:52 +0530

    Time: 61 ms (5.49%), Space: 108.7 MB (85.71%) - LeetHub

---
+++ 2216-minimum-deletions-to-make-array-beautiful/2216-minimum-deletions-to-make-array-beautiful.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minDeletion(int[] nums) {
+        int n=nums.length;
+        List<Integer> ans=new ArrayList<>();
+        ans.add(nums[0]);
+
+        for(int i=1;i<n;i++){
+            if(ans.size()%2!=0 && ans.get(ans.size()-1)==nums[i]) continue;
+            else ans.add(nums[i]);
+        }
+        System.out.println(ans);
+
+        int rem=n-ans.size();
+        if(ans.size()%2!=0) rem++;
+
+        return rem;
+    }
+}
