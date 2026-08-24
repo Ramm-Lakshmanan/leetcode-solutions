292d0ea3248a87501e649afa3f35f3a5b8143d78
Ramm_Lakshmanan <rockingramm27@gmail.com>
2026-08-24 22:37:45 +0530

    Time: 244 ms (100%), Space: 134.1 MB (0%) - LeetHub

--- 4032-longest-subarray-with-at-most-k-distinct-prime-factors/4032-longest-subarray-with-at-most-k-distinct-prime-factors.java
+++ 4032-longest-subarray-with-at-most-k-distinct-prime-factors/4032-longest-subarray-with-at-most-k-distinct-prime-factors.java
@@ -1,67 +1,51 @@
 class Solution {
-    static boolean[] prime=new boolean[100001];
-    static boolean built=false;
-    
-    public static void sieve(){
-        if(!built){
-            Arrays.fill(prime,true);
-            for(int i=2;i<=Math.sqrt(100000);i++){
-                if(prime[i]){
-                    for(int j=i*i;j<=100000;j+=i){
-                        prime[j]=false;
-                    }
+    public int longestSubarray(int[] nums, int k) {
+        int n=nums.length;
+        int max=-1;
+        for(int ele:nums) max=Math.max(max,ele);
+
+        int[] spf=new int[max+1];
+
+        for(int i=0;i<max+1;i++) spf[i]=i;
+
+        for(int i=2;i*i<=max;i++){
+            if(spf[i]==i){
+                for(int j=i*i;j<=max;j+=i){
+                    if(spf[j]==j) spf[j]=i;
                 }
             }
-            built=true;
-        }
-    }
-    
-    public List<Integer> pfactors(int num){
-        List<Integer> ans=new ArrayList<>();
-        if(num==1) return ans;
-        else if(prime[num]){
-            ans.add(num);
-            return ans;
-        }
-        
-        for(int i=2;i<=Math.sqrt(num);i++){
-            if(prime[i] && num%i==0){
-                ans.add(i);
-            }
-            if(i!=num/i && prime[(num/i)] && num%(num/i)==0) ans.add(num/i);
-        }
-        
-        return ans;
-    }
-    public int longestSubarray(int[] nums, int k) {
-        sieve();
-        List<List<Integer>> l=new ArrayList<>();
-        int n=nums.length;
-        
-        for(int i=0;i<n;i++){
-            List<Integer> cur=pfactors(nums[i]);
-            l.add(cur);
         }
 
-        
-        int left=0,right=0;
-        int ans=0;
+        List<Integer>[] fac=new ArrayList[n];
+
+        for(int i=0;i<n;i++) fac[i]=new ArrayList<>();
+
+        for(int i=0;i<n;i++){
+            int temp=nums[i];
+
+            while(temp>1){
+                int d=spf[temp];
+                fac[i].add(d);
+                while(temp%d==0) temp/=d;
+            }
+        }
 
         Map<Integer,Integer> hm=new HashMap<>();
 
+        int left=0,right=0,ans=0;
+
         while(right<n){
-            for(int fac:l.get(right)){
-                hm.put(fac,hm.getOrDefault(fac,0)+1);
+            for(int f:fac[right]){
+                hm.put(f,hm.getOrDefault(f,0)+1);
             }
 
             while(hm.size()>k){
-                for(int fac:l.get(left)){
-                    hm.put(fac,hm.get(fac)-1);
-                    if(hm.get(fac)==0) hm.remove(fac);
+                for(int f:fac[left]){
+                    hm.put(f,hm.get(f)-1);
+                    if(hm.get(f)==0) hm.remove(f);
                 }
                 left++;
             }
-
             ans=Math.max(ans,right-left+1);
             right++;
         }
