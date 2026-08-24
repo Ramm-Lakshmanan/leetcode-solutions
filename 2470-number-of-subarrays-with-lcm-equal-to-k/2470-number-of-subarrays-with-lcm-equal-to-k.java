114ed3eccac56e458da74d9cbeb07ce1e1691a0a
Ramm_Lakshmanan <rockingramm27@gmail.com>
2026-08-24 23:21:33 +0530

    Time: 16 ms (43.96%), Space: 43.8 MB (97.99%) - LeetHub

---
+++ 2470-number-of-subarrays-with-lcm-equal-to-k/2470-number-of-subarrays-with-lcm-equal-to-k.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int gcd(int a,int b){
+        while(a>0 && b>0){
+            if(a>b) a=a%b;
+            else b=b%a;
+        }
+
+        return (a==0)?b:a;
+    }
+    public int subarrayLCM(int[] nums, int k) {
+        int ans=0;
+
+        int n=nums.length;
+
+        for(int i=0;i<n;i++){
+            int lcm=nums[i];
+            if(lcm==k) ans++;
+
+            for(int j=i+1;j<n;j++){
+                int GCD=gcd(lcm,nums[j]);
+                int prod=lcm*nums[j];
+
+                lcm=prod/GCD;
+
+                if(lcm==k) ans++;
+                else if(lcm>k) break;
+            }
+        }
+
+        return ans;
+    }
+}
