f1ec2e12ccc4e35efb1c1785946fb74eeae0c194
Ramm_Lakshmanan <rockingramm27@gmail.com>
2026-08-28 13:45:10 +0530

    Time: 2 ms (28.22%), Space: 46.2 MB (95.44%) - LeetHub

---
+++ 0337-house-robber-iii/0337-house-robber-iii.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int func(TreeNode root,Map<TreeNode,Integer> hm){
+        if(root==null) return 0;
+
+        if(root.left==null && root.right==null) return root.val;
+
+        if(hm.containsKey(root)) return hm.get(root);
+
+        int ch1=root.val;
+
+        if(root.left==null) {
+            ch1+=func(root.right.left,hm)+func(root.right.right,hm);
+        }
+        else if(root.right==null){
+            ch1+=func(root.left.right,hm)+func(root.left.left,hm);
+        }
+        else{
+            ch1+=func(root.left.right,hm)+func(root.left.left,hm)+func(root.right.left,hm)+func(root.right.right,hm);
+        }
+
+        int ch2= func(root.left,hm)+func(root.right,hm);
+        hm.put(root,Math.max(ch1,ch2));
+        return Math.max(ch1,ch2);
+    }
+    public int rob(TreeNode root) {
+        Map<TreeNode,Integer> hm=new HashMap<>();
+
+        return func(root,hm);
+    }
+}
