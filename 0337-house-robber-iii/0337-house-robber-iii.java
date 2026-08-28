bf2475f3e011abcadfd04d42555f3c358fcbc15c
Ramm_Lakshmanan <rockingramm27@gmail.com>
2026-08-28 14:38:31 +0530

    Time: 9 ms (4.32%), Space: 47.3 MB (5.52%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.java
+++ 0337-house-robber-iii/0337-house-robber-iii.java
@@ -1,42 +1,52 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+import java.util.*;
+
 class Solution {
-    public int func(TreeNode root,Map<TreeNode,Integer> hm){
-        if(root==null) return 0;
 
-        if(root.left==null && root.right==null) return root.val;
+    public int func(TreeNode root, Map<TreeNode, Integer> hm) {
 
-        if(hm.containsKey(root)) return hm.get(root);
+        if (root == null) return 0;
 
-        int ch1=root.val;
-
-        if(root.right!=null) {
-            ch1+=func(root.right.left,hm)+func(root.right.right,hm);
-        }
-        if(root.left!=null){
-            ch1+=func(root.left.right,hm)+func(root.left.left,hm);
+        // If already calculated, return stored value
+        if (hm.containsKey(root)) {
+            return hm.get(root);
         }
 
-        int ch2= func(root.left,hm)+func(root.right,hm);
-        hm.put(root,Math.max(ch1,ch2));
-        return Math.max(ch1,ch2);
+        // Choice 1: Rob the current node
+        int ch1 = root.val;
+
+        if (root.left != null) {
+            ch1 += func(root.left.left, hm);
+            ch1 += func(root.left.right, hm);
+        }
+
+        if (root.right != null) {
+            ch1 += func(root.right.left, hm);
+            ch1 += func(root.right.right, hm);
+        }
+
+        // Choice 2: Don't rob the current node
+        int ch2 = func(root.left, hm) + func(root.right, hm);
+
+        int ans = Math.max(ch1, ch2);
+
+        hm.put(root, ans);
+
+        return ans;
     }
-    public int rob(TreeNode root) {
-        Map<TreeNode,Integer> hm=new HashMap<>();
 
-        return func(root,hm);
+    public int rob(TreeNode root) {
+
+        Map<TreeNode, Integer> hm = new TreeMap<>(
+            (a, b) -> {
+                if (a == b) return 0;
+
+                return Integer.compare(
+                    System.identityHashCode(a),
+                    System.identityHashCode(b)
+                );
+            }
+        );
+
+        return func(root, hm);
     }
 }
