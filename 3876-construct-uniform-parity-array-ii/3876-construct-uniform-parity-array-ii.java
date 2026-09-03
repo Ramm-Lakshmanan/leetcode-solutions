c0f845d43a188bc5bf90cfac42ff9d25aa0bbc12
Ramm_Lakshmanan <rockingramm27@gmail.com>
2026-09-03 11:55:46 +0530

    Time: 2 ms (100%), Space: 122 MB (8.7%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
@@ -1,8 +1,10 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
-        Arrays.sort(nums1);
+        int min=Integer.MAX_VALUE;
 
-        if(nums1[0]%2!=0) return true;
+        for(int ele:nums1) min=Math.min(ele,min);
+
+        if(min%2!=0) return true;
         else{
             for(int ele:nums1){
                 if(ele%2!=0) return false;
